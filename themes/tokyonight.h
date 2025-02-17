eaac4bd870f5fea095d9de168d6d19e2d776bec0
abdullah eskeif <abdullah.m.eskef@gmail.com>
2025-02-17 11:04:27 +0300

    before notitle

--- themes/tokyonight.h
+++ themes/tokyonight.h
@@ -1,9 +1,9 @@
-static const char normfgcolor[]     = "#cdd6f4";
+static const char normfgcolor[]     = "#ffffff";
 static const char normbgcolor[]     = "#000000";
-static const char normbordercolor[] = "#cdd6f4";
-static const char selbgcolor[]      = "#000000";
-static const char selbordercolor[]  = "#cdd6f4";
-static const char selfgcolor[]      = "#cdd6f4";
+static const char normbordercolor[] = "#000000";
+static const char selfgcolor[]      = "#000000";
+static const char selbordercolor[]  = "#000000";
+static const char selbgcolor[]      = "#89dceb";
 static const char *colors[][3]      = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
